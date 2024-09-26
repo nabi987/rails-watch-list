c3355231fc8d8ef8e4455983076b67f69608e8bf
Nabina Purja <npurja173@gmail.com>
2024-09-26 16:57:43 +0100

    lists completed and bookmarks added

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks, only: [:destroy]
 end
