2e813a3e5159377d6b4cea633611179501188d3a
Nabina Purja <npurja173@gmail.com>
2024-09-27 15:48:43 +0100

    bookmark delete option added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -15,6 +15,13 @@ class BookmarksController < ApplicationController
     end
   end
 
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @list = @bookmark.list
+    @bookmark.destroy
+    redirect_to list_path(@list), status: :see_other
+  end
+
   private
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
